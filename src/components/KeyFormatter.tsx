5141068986e650320401f8fc5ba4c25b3463cffd
Wai Min <leopard23@gmail.com>
2025-08-17 16:41:54 +0800

    Implement single line conversion

--- src/components/KeyFormatter.tsx
+++ src/components/KeyFormatter.tsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
 import KeyInput from './KeyInput';
+import KeySingleLineOutput from './KeySingleLineOutput';
 
 const KeyFormatter = () => {
-  return <KeyInput />;
+  const [originalText, setOriginalText] = useState('');
+  return (
+    <div className='container'>
+      <h1 className='p-2 mb-16 text-4xl font-bold text-center text-purple-600 shadow-3xl'>
+        Key Formatter
+      </h1>
+      <KeyInput setOriginalText={setOriginalText} />
+      <KeySingleLineOutput originalText={originalText} />
+    </div>
+  );
 };
 
 export default KeyFormatter;
--- src/components/KeyInput.tsx
+++ src/components/KeyInput.tsx
@@ -1,26 +1,37 @@
 import { useState } from 'react';
 
-const KeyInput = () => {
+const KeyInput = (props: { setOriginalText: (text: string) => void }) => {
   const placeHolderText = `Enter your key here..`;
-  const [originalText, setOriginalText] = useState('');
-
+  const [text, setText] = useState('');
 
   const pasteFromClipboard = () => {
-	navigator.clipboard.readText().then(text => {
-	  setOriginalText(text);
-	});
+    navigator.clipboard.readText().then((text) => {
+      setText(text);
+      props.setOriginalText(text);
+    });
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const text = e.target.value;
+    setText(text);
+    props.setOriginalText(text);
   };
 
   return (
-    <div className='w-full h-64'>
+    <div className='w-full h-64 mb-32'>
       <header className='flex justify-between items-center mb-4'>
-        <h2 className='text-2xl font-bold mb-4'>Key Formatter</h2>
-        <button onClick={pasteFromClipboard} className='bg-purple-500 text-white px-4 py-2 rounded hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-400'>
+        <h2 className='text-2xl font-bold mb-4'>Original Key</h2>
+        <button
+          onClick={pasteFromClipboard}
+          className='bg-purple-500 text-white px-4 py-2 rounded hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-400'
+        >
           Paste
         </button>
       </header>
-      <textarea value={originalText} onChange={(e) => setOriginalText(e.target.value)}
-        className='w-full h-56 p-2 border-2 border-purple-400 hover:border-purple-500 focus:outline-none focus:ring-1 focus:ring-purple-600'
+      <textarea
+        value={text}
+        onChange={handleChange}
+        className='w-full h-full p-2 border-2 border-purple-400 hover:border-purple-500 focus:outline-none focus:ring-1 focus:ring-purple-600'
         placeholder={placeHolderText}
       />
     </div>
